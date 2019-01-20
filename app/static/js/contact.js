882a5f22d31d3d052d4487fa9b6160a09d595dd4
patryk <psambek@gmail.com>
2019-01-20 01:58:06 +0100

    Map improved.

--- app/static/js/contact.js
+++ app/static/js/contact.js
@@ -3,7 +3,7 @@
 
     $('textarea').characterCounter();
 
-    const mymap = L.map('map-wrapper').setView([51.2196492, 22.695445], 15);
+    const mymap = L.map('map-wrapper').setView([51.2196492, 22.695400], 14);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
